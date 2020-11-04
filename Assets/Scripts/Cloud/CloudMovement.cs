5b66f8eefa5ec253426c983f9e55d00d864c7218
Joy Goldar <joy211191@gmail.com>
2020-11-04 14:28:02 +0100

    Cloud

---
+++ Assets/Scripts/Cloud/CloudMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    [SerializeField]
+    float sideMovementForce;
+    [SerializeField]
+    float jumpForce;
+
+    private void Awake() {
+        rb2D = GetComponent<Rigidbody2D>();   
+    }
+
+    private void FixedUpdate() {
+        float h = Input.GetAxis("Horizontal");
+        rb2D.AddForce(Vector2.right* sideMovementForce * h);
+
+        if (Input.GetButton("Jump")) {
+            rb2D.AddForce(Vector2.up * jumpForce);
+        }
+        
+    }
+}
