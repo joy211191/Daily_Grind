b5ff7fb4c160fcd4295d1c54c93cf1ecd80c7b22
Joy Goldar <joy211191@gmail.com>
2020-11-04 15:07:09 +0100

    Pushed the goddamn project

--- Assets/Scripts/Cloud/CloudMovement.cs
+++ Assets/Scripts/Cloud/CloudMovement.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class CloudMovement : MonoBehaviour
 {
+    public float burnRateMultiplier=1;
+    [SerializeField]
+    float baseBurnRate=10;
+    float energy=100;
+    [SerializeField]
+    [Range(0f, 1f)]
+    float zeroEnergyMoveSpeed;
     Rigidbody2D rb2D;
     [SerializeField]
-    float sideMovementForce;
+    float horizontalMovement;
     [SerializeField]
-    float jumpForce;
+    float verticalMovement;
+
+    [SerializeField]
+    GameObject trailObject;
 
     private void Awake() {
-        rb2D = GetComponent<Rigidbody2D>();   
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
         float h = Input.GetAxis("Horizontal");
-        rb2D.AddForce(Vector2.right* sideMovementForce * h);
+        float v = Input.GetAxis("Vertical");
 
-        if (Input.GetButton("Jump")) {
-            rb2D.AddForce(Vector2.up * jumpForce);
+        trailObject.SetActive(rb2D.velocity.magnitude > 0.2f);
+
+
+        if (energy > 0) {
+            rb2D.AddForce(Vector2.right * horizontalMovement * h);
+            rb2D.AddForce(Vector2.up * verticalMovement * v);
+            if (Mathf.Abs(h) > 0||Mathf.Abs(v)>0)
+                energy -= Time.deltaTime * baseBurnRate * burnRateMultiplier;
         }
-        
+        else {
+            bool jump = Input.GetButtonDown("Jump");
+            rb2D.AddForce(Vector2.right * horizontalMovement * h* zeroEnergyMoveSpeed);
+            if(jump)
+                rb2D.AddForce(Vector2.up * verticalMovement * zeroEnergyMoveSpeed);
+        }
     }
 }
